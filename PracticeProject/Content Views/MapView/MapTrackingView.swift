e7eb722cc2dc6de6208899ef105f0a6084963eeb
Mahesh Babu <maheshpittala32@gmail.com>
2024-01-26 17:52:55 +0530

    Fixed issues in map

--- PracticeProject/Content Views/MapView/MapTrackingView.swift
+++ PracticeProject/Content Views/MapView/MapTrackingView.swift
@@ -11,31 +11,49 @@ import CoreLocation
 
 struct MapTrackingView: View {
     
-    let myCurrentLocation = CLLocationCoordinate2D(latitude:23.011336543822395 , longitude: 72.51981302500232)
-    @State var camera: MapCameraPosition = .automatic
+    @State private var userCurrentLocation: CLLocationCoordinate2D?
+    @State private var camera: MapCameraPosition = .automatic
     
     var body: some View {
         
-        ZStack(alignment: .bottomTrailing){
-            Map(position: $camera){
-                Marker("Home",systemImage: "shareplay",coordinate: myCurrentLocation)
-            }.mapStyle(.standard)
-                Button{
-                    camera = .region(MKCoordinateRegion(center: myCurrentLocation, latitudinalMeters: 200, longitudinalMeters: 200))
-                }label: {
-                    Image(systemName: "paperplane").resizable()
-                        .foregroundStyle(Color.black)
-                        .padding(.all,10)
-                }
-                
-                .frame(width: 40,height: 40)
-                .cornerRadius(5)
-                .background(Color.white)
-            .padding()
+        ZStack(alignment: .bottomTrailing) {
             
+            Map(position: $camera) {
+                // Add a default marker using the user's current location
+                if let location = userCurrentLocation {
+                    Marker("Your Location", systemImage: "location", coordinate: location)
+                }
+            }.mapStyle(.standard)
+            
+            // Button to set the camera to the user's current location
+            Button {
+                if let location = userCurrentLocation {
+//                    camera = .region(MKCoordinateRegion(center: location, latitudinalMeters: 200, longitudinalMeters: 200))
+                }
+                camera = .userLocation(fallback: .automatic)
+
+            } label: {
+                Image(systemName: "paperplane.fill")
+                    .resizable()
+                    .foregroundStyle(Color.btnGradientColor)
+                    .padding(.all, 10)
+            }
+            .frame(width: 40, height: 40)
+            .cornerRadius(10)
+            .background(Color.mainColor)
+            .padding()
+        }
+        .onAppear {
+            fetchUserLocation()
+            camera = .userLocation(fallback: .automatic)
         }
     }
+    private func fetchUserLocation() {
+        let locationManager = CLLocationManager()
+        locationManager.requestWhenInUseAuthorization()
+    }
 }
+
 
 #Preview {
     MapTrackingView()
