8bdaf5d5f97fe3047a69d66f30e02fc271facb1d
MaheshBabu-Thinkbiz <maheshbabu@vebuin.com>
2024-02-22 12:46:54 +0530

    Added new features

--- PracticeProject/Views/Content Views/HomeGroups/HomeMainView.swift
+++ PracticeProject/Views/Content Views/HomeGroups/HomeMainView.swift
@@ -13,37 +13,73 @@ struct HomeMainView: View {
     @State private var arrCards : [String] = ["Healthy Foods", "Unhealthy Food", "High in protien", ]
     
     @State private var cards : [FoodCardRow] = []
+    @State private var profilePhotoTapped : Bool = false
+    @Namespace private var nameSpace
     
     var body: some View {
-        NavigationStack{
-            ZStack{
-                ScrollView(.vertical, showsIndicators: false, content: {
-                    LazyVGrid(columns: [GridItem()], content: {
-                        ForEach(cards) { card in
-                            FoodCardView(height: 400,
-                                         strCardTitle: card.cardTitle,
-                                         strCardHeadLine: card.cardHeadTitle, strCardDescription: card.cardDesc, expandButtonHandler: {
-                                self.isSheetExpanded.toggle()
-                               
-                            }, backgroundImageString: card.backgroundImageString, customBackgroundColor: card.backgroundGradient)
-                            .fullScreenCover(isPresented: $isSheetExpanded,onDismiss: {
-                                addData()
-                            }, content: {
-                                
-                                HomeDetailView(closeButtonHandler: {
+        ZStack{
+            NavigationStack{
+                ZStack{
+                    ScrollView(.vertical, showsIndicators: false, content: {
+                        LazyVGrid(columns: [GridItem()], content: {
+                            ForEach(cards) { card in
+                                FoodCardView(height: 400,
+                                             strCardTitle: card.cardTitle,
+                                             strCardHeadLine: card.cardHeadTitle, strCardDescription: card.cardDesc, expandButtonHandler: {
                                     self.isSheetExpanded.toggle()
-                                }, backgroundGredient: card.backgroundGradient, imageString: card.backgroundImageString, imageHeight: 350)
-                            }) .transition(.move(edge: .trailing))
+                                    
+                                }, backgroundImageString: card.backgroundImageString, customBackgroundColor: card.backgroundGradient)
+                                .fullScreenCover(isPresented: $isSheetExpanded,onDismiss: {
+                                    addData()
+                                }, content: {
+                                    
+                                    HomeDetailView(closeButtonHandler: {
+                                        self.isSheetExpanded.toggle()
+                                    }, backgroundGredient: card.backgroundGradient, imageString: card.backgroundImageString, imageHeight: 350)
+                                }) .transition(.move(edge: .trailing))
+                            }
+                        })
+                    })
+                }.padding(.horizontal, 5)
+                    .navigationTitle("Home")
+                    .toolbar(content: {
+                        ToolbarItem(placement: .topBarTrailing) {
+                            Image("flower")
+                                .resizable()
+                                .clipShape(Circle())
+                                .matchedGeometryEffect(id: "image", in: nameSpace)
+                                .scaledToFit()
+                                .frame(width: 75, height: 75)
+                                .onTapGesture {
+                                    withAnimation(.spring) {
+                                        profilePhotoTapped.toggle()
+                                    }
+                                }
                         }
                     })
-                })
-            }.padding(.horizontal, 5)
-                .navigationTitle("Home")
+            }
+            .onAppear(perform: {
+                addData()
+            })
+            
+            if profilePhotoTapped{
+                ZStack {
+                    Color.black.opacity(0.5)
+                        .ignoresSafeArea()
+                    Image("flower")
+                        .resizable()
+                        .clipShape(Circle())
+                        .matchedGeometryEffect(id: "image", in: nameSpace)
+                        .scaledToFill()
+                        .frame(width: 250, height: 250)
+                }.onTapGesture {
+                    withAnimation(.spring) {
+                        profilePhotoTapped.toggle()
+                    }
+                }
+            }
         }
-        .onAppear(perform: {
-            addData()
-        })
-    }    
+    }
     
     func addData(){
         cards = [
