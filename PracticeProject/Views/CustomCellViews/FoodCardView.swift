98e49911e2b4a55a56a23b90e9c2f4901fd570ce
MaheshBabu-Thinkbiz <maheshbabu@vebuin.com>
2024-02-23 18:03:46 +0530

    fixed error

--- PracticeProject/Views/CustomCellViews/FoodCardView.swift
+++ PracticeProject/Views/CustomCellViews/FoodCardView.swift
@@ -26,31 +26,6 @@ struct FoodCardView: View {
                         .scaledToFit()
                         .frame(height: height*3/5)
                     
-                    VStack{
-                            Image("\(backgroundImageString)")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(height: height*3/5)
-                        
-                        VStack(alignment: .leading, spacing: 10){
-//                            Text("\(strCardTitle)")
-//                                .bold()
-//                                .multilineTextAlignment(.leading)
-//                                .foregroundStyle(Color.white)
-//                                .fontWeight(.medium)
-//                                .lineLimit(1)
-//                                .font(.title)
-                            Text("\(strCardDescription)")
-                                .multilineTextAlignment(.leading)
-                                .foregroundStyle(Color.white)
-                                .lineLimit(2)
-                                .font(.headline)
-                                .bold()
-                                .minimumScaleFactor(0.2)
-
-                        }.padding(.bottom, 0)
-                        .padding(.horizontal)
-                    }.frame(height: height)
                 }.frame(height: height)
                 Text("\(strCardDescription)")
                     .multilineTextAlignment(.leading)
